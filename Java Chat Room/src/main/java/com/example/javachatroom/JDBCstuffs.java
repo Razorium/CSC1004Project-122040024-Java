30d8d1c49f68ef75b2b7ba81024e1b03bd0b63cb
Razorium <jonathanktj.jt@gmail.com>
2023-04-07 20:32:39 +0800

    mysql connection and login systme

--- Java Chat Room/src/main/java/com/example/javachatroom/JDBCstuffs.java
+++ Java Chat Room/src/main/java/com/example/javachatroom/JDBCstuffs.java
@@ -14,7 +14,7 @@ public class JDBCstuffs {
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "Jo73n5634434#";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/newsSystem?allowPublicKeyRetrieval=true&useSSL=false&characterEncoding=utf-8";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/ok1?allowPublicKeyRetrieval=true&useSSL=false&characterEncoding=utf-8";
 
 
     @FXML
@@ -30,15 +30,36 @@ public class JDBCstuffs {
         String username1=username.getText();
         String password1=password.getText();
         System.out.println(username1+" dengan password "+password1);
-    }
+
+        try(Connection conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);) {
+
+            String sql = "select * from `usnpass` where usn = ? and pass = ?";
+            List<Object> params = new ArrayList<>();
+            params.add(username1);
+            params.add(password1);
+
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setObject(1, params.get(0));
+            pstmt.setObject(2, params.get(1));
+            resultSet = pstmt.executeQuery();
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int col = metaData.getColumnCount();
+            int counter=0;
+            while (resultSet.next()) {
+                counter++;
+            }
+            if (counter==0){
+                System.out.println("Either password or username is incorrect.");
+            }else{
+                System.out.println("It worked!!!!!");
+
+            }
 
 
-    @FXML
-    private void closeButtonAction(){
-        // get a handle to the stage
-        Stage stage = (Stage) cancelbut.getScene().getWindow();
-        // do what you have to do
-        stage.close();
+            System.out.println("Connected database successfully...");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
